53b15406b1013de9fe900ec61a6f209199af04a9
Phil Nocerini <nocerini@gmail.com>
2023-09-11 18:31:37 -0400

    More Monday Assignments

---
+++ video-preview/script.js
@@ -0,0 +1,11 @@
+console.log("page loaded...");
+
+function playVideo(elememt){
+    elememt.muted = true
+    elememt.play()
+}
+
+function pauseVideo(elememt){
+    elememt.muted = true
+    elememt.pause()
+}
